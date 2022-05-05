6c6484812fd839ca2cbf3221a405e47edb674b3c
hasibkyau <hasibkyau.cse@gmail.com>
2022-05-05 10:48:03 +0600

    added a Motor Class for motor controlling

---
+++ Scorpion.h
@@ -0,0 +1,72 @@
+#ifndef MOTOR_H
+#define MOTOR_H
+#include <Arduino.h>
+
+class Motor {
+  private:
+    int in1;
+    int in2;
+    int enable1Pin;
+    const int freq = 30000;
+    const int pwmChannel = 0;
+    const int resolution = 8;
+    
+  public:
+    int dutyCycle = 200;
+    Motor(int in1, int in2, int enable1Pin) {
+      this -> in1 = in1;
+      this -> in2 = in2;
+      this -> enable1Pin = enable1Pin;
+      init();
+    };
+
+    void init() {
+      // sets the pins as outputs:
+      pinMode(in1, OUTPUT);
+      pinMode(in2, OUTPUT);
+      pinMode(enable1Pin, OUTPUT);
+
+      // configure LED PWM functionalitites
+      ledcSetup(pwmChannel, freq, resolution);
+
+      // attach the channel to the GPIO to be controlled
+      ledcAttachPin(enable1Pin, pwmChannel);
+      
+      ledcWrite(pwmChannel, dutyCycle);
+    }
+
+    void Forward() {
+      Serial.println("Forward");
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, HIGH);
+    };
+
+    void Backward() {
+      Serial.println("Backward");
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+    };
+
+    void Stop() {
+      Serial.println("Stop");
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, LOW);
+    };
+
+    void Status(){
+      Serial.println("Pin used : ");
+      Serial.println(in1);
+      Serial.println(in2);
+      Serial.println(enable1Pin);
+    }
+    void Speed(int spd){
+      this -> dutyCycle = spd;
+      ledcWrite(pwmChannel, dutyCycle);
+      Serial.println("Current Speed : ");
+      Serial.println(dutyCycle);
+      }
+};
+
+
+
+#endif
--- Scorpion.ino
+++ Scorpion.ino
@@ -1,10 +1,22 @@
-#include "motor.h"
+#include "Scorpion.h"
+
+//Using class "Motor" {methods = Forward, Backward, Stop, Speed, Status}
+Motor motorA(27, 26, 14);  //(in1, in2, en)
+Motor motorB(4, 5, 13);  //(in1, in2, en)
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
 
+  
+  Serial.println("After modification :");
+  motorA.Forward();
+  delay(2000);
+
+  motorB.Speed(255);
+  delay(2000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  motor_driver();
+
 }
